fcc0d225ded4325bef67de0810fc26492a6589a7
Sree0503 <122594220+Sree0503@users.noreply.github.com>
2025-05-22 09:56:33 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { AppSidebar } from "@/components/app-sidebar"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
+import { Card } from '@/components/ui/card'; 
+
 import {
   SidebarInset,
   SidebarProvider,
